f3097cfdc90df2abc28e419125d84fc30f0ba289
emma-burrows <emma@astele.co.uk>
2013-09-07 23:38:06 +0100

    Removed prototype models; added first working EF-generated controller and models

---
+++ MvcLamarelle/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcLamarelle.Models
+{
+
+  public class Contact
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int ID { get; set; }
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+  }
+
+  public class ContactsDBContext : DbContext
+  {
+    public ContactsDBContext() : base("DefaultConnection") {}
+
+    public DbSet<Contact> Contacts { get; set; }
+  }
+}
