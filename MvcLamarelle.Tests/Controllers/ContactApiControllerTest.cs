e74c8cddf87af694bcc7c1b687ab3451a79b04ad
Emma Burrows <emma@astele.co.uk>
2014-01-17 09:45:04 +0000

    Started work on a little API controller.

---
+++ MvcLamarelle.Tests/Controllers/ContactApiControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Collections.Generic;
+
+namespace MvcLamarelle.Tests.Controllers
+{
+  [TestClass]
+  public class ContactApiControllerTest
+  {
+    private HttpClient _client;
+
+    [TestInitialize]
+    public void TestInitialise()
+    {
+      _client = ApiClient.GetAuthenticatedClient();
+    }
+
+    [TestMethod]
+    public void contactIndexReturnsStringArray()
+    {
+      var list = _client
+                  .GetAsync("api/ContactApi")
+                  .Result.Content.ReadAsAsync<IEnumerable<string>>()
+                  .Result;
+
+      Assert.IsTrue(list.Any());
+    }
+  }
+}
---
+++ MvcLamarelle/Controllers/ContactApiController.cs
@@ -0,0 +1,43 @@
+﻿using MvcLamarelle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcLamarelle.Controllers
+{
+  public class ContactApiController : ApiController
+  {
+    private ContactsDBContext db = new ContactsDBContext();
+
+    // GET api/contactapi
+    public IEnumerable<string> Get()
+    {
+      string[] contacts = db.Contacts.ToList().Select(c => c.Nom).ToArray();
+      return contacts ;
+    }
+
+    // GET api/contactapi/5
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/contactapi
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/contactapi/5
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/contactapi/5
+    public void Delete(int id)
+    {
+    }
+  }
+}
